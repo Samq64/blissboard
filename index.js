70058db13b1d190def51d9c6c92e2c5fa8f61750
Samq64 <81489795+Samq64@users.noreply.github.com>
2024-05-13 17:12:39 -0400

    Add styling and history

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 async function main() {
     const input = document.getElementById("input");
     const output = document.getElementById("output");
+    const list = document.getElementById("list");
     const words = await fetch("words.json").then(res => res.json());
 
     input.addEventListener("input", () => {
-        document.getElementById("output").textContent = words[input.value];
+        const number = input.value;
+        const word = words[number];
+        if (word) {
+            output.textContent = `Word: ${word}`;
+            const el = document.createElement("li");
+            el.textContent = word;
+            list.appendChild(el);
+        } else if (number.length === 4) {
+            output.textContent = "No word found";
+        } else if (number.length > 4) {
+            input.value = number.slice(4);
+        }
     });
 }
 
