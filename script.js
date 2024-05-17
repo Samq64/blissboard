ef57103e17d588a1ac01b654922d056993f09b8c
Samq64 <81489795+Samq64@users.noreply.github.com>
2024-05-16 21:45:02 -0400

    Prettier

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 async function main() {
-    const input = document.getElementById("input");
-    const output = document.getElementById("output");
-    const list = document.getElementById("list");
-    const words = await fetch("words.json").then(res => res.json());
+  const input = document.getElementById("input");
+  const output = document.getElementById("output");
+  const list = document.getElementById("list");
+  const words = await fetch("words.json").then((res) => res.json());
 
-    input.addEventListener("input", () => {
-        const number = input.value;
-        const word = words[number];
-        if (word) {
-            output.textContent = word;
-            const el = document.createElement("li");
-            const span = document.createElement("span");
-            el.textContent = word;
-            span.style.float = "right";
-            span.textContent = number;
-            el.appendChild(span);
-            list.insertBefore(el, list.children[0]);
-        } else if (number.length === 4) {
-            output.textContent = "No word found";
-        } else if (number.length > 4) {
-            input.value = number.slice(4);
-        }
-    });
+  input.addEventListener("input", () => {
+    const number = input.value;
+    const word = words[number];
+    if (word) {
+      output.textContent = word;
+      const el = document.createElement("li");
+      const span = document.createElement("span");
+      el.textContent = word;
+      span.style.float = "right";
+      span.textContent = number;
+      el.appendChild(span);
+      list.insertBefore(el, list.children[0]);
+    } else if (number.length === 4) {
+      output.textContent = "No word found";
+    } else if (number.length > 4) {
+      input.value = number.slice(4);
+    }
+  });
 }
 
 main();
