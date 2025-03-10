24f7fd4c6720eef89c86a7b4115effe800453288
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-03-10 16:40:08 -0400

    Style tweaks

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ async function main() {
       const el = document.createElement("li");
       const span = document.createElement("span");
       el.textContent = word;
-      span.style.float = "right";
       span.textContent = number;
       el.appendChild(span);
       list.insertBefore(el, list.children[0]);
