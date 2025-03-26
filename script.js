8d06b371b96cf3471df9b69aba3cddeafe981f07
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-03-26 12:40:31 -0400

    Fetch from JS file imstead of JSON
    The old file was kept for compatibility with the Scratch version.

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-async function main() {
-  const input = document.getElementById("input");
-  const output = document.getElementById("output");
-  const list = document.getElementById("list");
-  const words = await fetch("words.json").then((res) => res.json());
+import words from "./words.js";
 
-  input.addEventListener("input", () => {
-    const number = input.value;
-    const word = words[number];
-    if (word) {
-      output.textContent = word;
-      const el = document.createElement("li");
-      const span = document.createElement("span");
-      el.textContent = word;
-      span.textContent = number;
-      el.appendChild(span);
-      list.insertBefore(el, list.children[0]);
-      document.getElementById("history").style.display = "block";
-    } else if (number.length === 4) {
-      output.textContent = "No word found";
-    } else if (number.length > 4) {
-      input.value = number.slice(4);
-    }
-  });
-}
+const output = document.getElementById("output");
+const input = document.getElementById("input");
+input.value = "";
 
-main();
+input.addEventListener("input", () => {
+  const number = input.value;
+  const word = words[number];
+  if (word) {
+    output.textContent = word;
+    const el = document.createElement("li");
+    const span = document.createElement("span");
+    el.textContent = word;
+    span.textContent = number;
+    el.appendChild(span);
+    document.getElementById("list").prepend(el);
+    document.getElementById("history").style.display = "block";
+  } else if (number.length === 4) {
+    output.textContent = "No word found";
+  } else if (number.length > 4) {
+    input.value = number.slice(4);
+  }
+});
