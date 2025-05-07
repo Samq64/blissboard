ac0482a23f2ab2e0422b00677cc9b5055037ddd5
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-05-07 11:40:09 -0400

    Fix cache

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 const cacheName = "blissboard-v1";
-const cacheFiles = [
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/words.js",
-  "/images/eyes.svg",
-  "/images/eyes-dark.svg",
-  "/images/character.svg",
+const assets = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "words.js",
+  "images/eyes.svg",
+  "images/eyes-dark.svg",
+  "images/character.svg",
 ];
 
 self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install");
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
-      return cache.addAll(cacheFiles);
+      return cache.addAll(assets);
     })
   );
 });
